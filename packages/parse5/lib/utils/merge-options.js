7a99a7dda38d8462823c58916d7242f00f42ae53
Frank Lemanschik <frank@dspeed.eu>
2021-02-03 09:19:57 +0100

    Replace own mergeOptions with nativ Object.assign

--- packages/parse5/lib/utils/merge-options.js
+++ packages/parse5/lib/utils/merge-options.js
@@ -1,13 +1,3 @@
 'use strict';
 
-module.exports = function mergeOptions(defaults, options) {
-    options = options || Object.create(null);
-
-    return [defaults, options].reduce((merged, optObj) => {
-        Object.keys(optObj).forEach(key => {
-            merged[key] = optObj[key];
-        });
-
-        return merged;
-    }, Object.create(null));
-};
+module.exports = (defaults,options) => Object.assign(defaults,options || {});
